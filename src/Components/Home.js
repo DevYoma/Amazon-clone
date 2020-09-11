8deb37c4fb9b15ef0f6397582a620ef735a1b714
DevYoma <Lawrenceyoma@gmail.com>
2020-09-11 21:40:20 +0100

    added pictures

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,11 @@ import './Home.css'
 import Product from '../Components/Product'
 import Amazon from '../assets/amazon.jpg';
 import LeanStartUp from '../assets/LeanStartUp.jpg';
+import Ps4 from '../assets/Ps4.jpg';
+import Laptop from  '../assets/laptop.jpg';
+import Blender from '../assets/blender.jpg';
+import Tv from '../assets/Tv.jpg';
+import Bitcoin from '../assets/Bitcoin.jpg';
 
 
 const Home = () => {
@@ -18,49 +23,49 @@ const Home = () => {
             <div className="home__row">
                 <Product
                     id="12321341"
-                    title="The Lean StartUp"
-                    price={11.96}
+                    title="PlayStation 4 Gaming Console"
+                    price={400.50}
                     rating={5}
-                    image={LeanStartUp}
+                    image={Ps4}
                 />
 
                 <Product
                     id="43448583"
-                    title="The Lean wizard of Oz"
-                    price={110.96}
-                    rating={4}
-                    image={LeanStartUp}
+                    title="Dell XPS 15"
+                    price={500.96}
+                    rating={5}
+                    image={Laptop}
                 />
             </div>
 
             <div className="home__row">
                 <Product
                     id="12326661"
-                    title="The Lean StartUp of Padua"
+                    title="Blender for grinding pepper 🤣"
                     price={150.96}
-                    rating={4}
-                    image={LeanStartUp}
+                    rating={3}
+                    image={Blender}
                 />
                  <Product
                     id="12320303"
-                    title="The Lean StartUp of Magella"
-                    price={50.96}
-                    rating={3}
-                    image={LeanStartUp}
+                    title="Smart Television with Hifi"
+                    price={900.96}
+                    rating={6}
+                    image={Tv}
                 />
                  <Product
                     id="12454341"
-                    title="The Lean StartUp of Italy"
-                    price={1}
-                    rating={1}
-                    image={LeanStartUp}
+                    title="Bitcoin 😲"
+                    price={1000000}
+                    rating={7}
+                    image={Bitcoin}
                 />
             </div>
 
             <div className="home__row">
                 <Product
                     id="12321541"
-                    title="The Lean StartUp of Time"
+                    title="The Lean StartUp"
                     price={1000.96}
                     rating={5}
                     image={LeanStartUp}
